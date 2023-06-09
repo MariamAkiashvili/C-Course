c8948bf71b59e84c03feafe578546c994d753783
Mariam Akiashvili <76559977+MariamAkiashvili@users.noreply.github.com>
2023-06-09 19:46:14 +0400

    Add files via upload

---
+++ HomeWork03/Task01/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+Console.WriteLine("Enter the lenght of array: ");
+int length = int.Parse(Console.ReadLine());
+
+int[] array = new int[length];
+for (int i = 0; i< length; i++)
+{
+    array[i] = int.Parse(Console.ReadLine());
+}
+
+
+for (int j = 0; j < length; j+=2)
+{
+
+    for (int i = 1; i < length; i++)
+    {
+
+        if (array[i - 1] > array[i])
+        {
+            int x = array[i - 1];
+            array[i - 1] = array[i];
+            array[i] = x;
+        }
+    }
+}
+
+
+foreach(int i in array)
+{
+    Console.WriteLine(i);
+}
+
+Console.ReadLine();
+
+
+
