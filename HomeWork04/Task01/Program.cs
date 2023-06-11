8787b3908bd41bbdca42a4c1205373b0cdf43bee
Mariam <mari.akaishvili@gmail.com>
2023-06-11 12:47:32 +0400

    Assignment04

---
+++ HomeWork04/Task01/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter the number of array elements");
+
+int length = int.Parse(Console.ReadLine());
+int[] arr = new int[length];
+
+for (int i = 0; i < length; i++)
+{
+    Console.WriteLine("Enter the element:");
+    arr[i] = int.Parse(Console.ReadLine());
+}
+Console.WriteLine("Enter the index of the element between 0 "+(arr.Length-1)+" : ");
+int index = int.Parse(Console.ReadLine());
+if (index >= arr.Length)
+{
+    Console.WriteLine("Index is out of array.");
+}
+else
+{
+    Console.WriteLine(SumOfDigits(arr, index));
+}
+
+
+
+int SumOfDigits(int[] arr, int index)
+{
+    int sum = 0;
+    int number = arr[index];
+    while (number > 0)
+    {
+        sum += number % 10;
+        number /= 10;
+    }
+    return sum;
+}
+
+
+
+Console.ReadLine();
---
+++ HomeWork04/Task02/Program.cs
@@ -0,0 +1,68 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+char[] arr = Func1();
+
+char elem;
+
+while (true)
+{
+    Console.WriteLine("Enter the character you want to find in array: ");
+
+    if (!char.TryParse(Console.ReadLine(), out elem))
+    {
+        Console.WriteLine("Please enter only one character!");
+    }
+    else
+    {
+        Func3(Func2(arr, elem), elem);
+        break;
+    }
+}
+
+
+
+
+Console.ReadLine();
+
+
+
+
+
+char[] Func1()
+{
+    Console.WriteLine("Enter the number of array elements");
+    int length = int.Parse(Console.ReadLine());
+    char[] arr = new char[length];
+    char val;
+    for (int i = 0; i < length; i++)
+    {
+        Console.WriteLine("Enter the element:");
+        if(char.TryParse(Console.ReadLine(), out val))
+        {
+            arr[i] = val;
+        }
+        else
+        {
+            Console.WriteLine("Please input only one character!");
+            i--;
+        }
+        
+    }
+
+    return arr;
+}
+
+int Func2(char[] arr, char elem)
+{
+    int num = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        num += arr[i] == elem ? 1 : 0;
+    }
+    return num;
+}
+
+void Func3 (int num, char elem)
+{
+    Console.WriteLine("'"+elem+"' occurs "+num+" times");
+}
